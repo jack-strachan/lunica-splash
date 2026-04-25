5215affff187ed040e0afff5ccdb0fb0dbc5af5c
Jack Strachan <strachan@scopelabs.com>
2026-04-24 22:29:51 -0400

    Fix OG: use lunica-splash.vercel.app as default base URL

--- src/content/site.ts
+++ src/content/site.ts
@@ -1,7 +1,7 @@
 const getBaseUrl = () => {
   if (process.env.NEXT_PUBLIC_SITE_URL) return process.env.NEXT_PUBLIC_SITE_URL
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
-  return "https://lunica.com"
+  return "https://lunica-splash.vercel.app"
 }
 
 export const siteConfig = {
